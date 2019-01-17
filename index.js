32eb5d7efc669773959a274951e5843f9f29a512
Jang Si <jang.si@toppick.co>
2019-01-16 22:03:40 -0500

    use node mailer to compose email with attachments

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var gmailApiSync = require('gmail-api-sync');
 var google = require('googleapis');
+var MailComposer = require("nodemailer/lib/mail-composer");
 
 exports.setClientSecretsFile = function (path) {
     gmailApiSync.setClientSecretsFile(path);
@@ -14,39 +15,46 @@ exports.send = function (token, params, callback) {
             return callback('Auth error: ' + err, null);
         }
         var gmail = google.gmail('v1');
-        var headers = [];
+        // var headers = [];
 
-        headers.push('From: <' + params.from + '>');
-        headers.push('To: ' + params.to);
-        headers.push('Content-type: text/html;charset=iso-8859-1');
-        headers.push('MIME-Version: 1.0');
-        headers.push('Subject: ' + params.subject);
-        headers.push('');
-        headers.push(params.body);
+        var mail = new MailComposer({
+            from: params.from,
+            to: params.to,
+            subject: params.subject,
+            html: params.body,
+            attachments: params.attachments.map(a => {
+                return {
+                    filename: a.name,
+                    content: a.base64Content,
+                    encoding: 'base64'
+                }
+            })
+        });
 
-        var email = headers.join('\r\n').trim();
+        mail.compile().build(function(err, message) {
+            if (err) {
+                return callback('Unable to compile message: ' + err, null);
+            } else {
+                var reqParams = {
+                    auth: oauth,
+                    userId: 'me',
+                    resource: {
+                        raw: message.toString('base64').replace(/\+/g, '-').replace(/\//g, '_')
+                    }
+                };
 
-        var base64EncodedEmail = new Buffer(email).toString('base64');
-        base64EncodedEmail = base64EncodedEmail.replace(/\+/g, '-').replace(/\//g, '_');
-
-        var reqParams = {
-            auth: oauth,
-            userId: 'me',
-            resource: {
-                raw: base64EncodedEmail,
-            }
-        };
-
-        if (params.threadId) {
-            reqParams.resource.threadId = params.threadId;
-        }
-
-        gmail.users.messages.send(reqParams, null, function (err, resp) {
-            if (!err) {
-                return callback(null, resp);
-            }
-            else {
-                return callback('Unable to send email: ' + err, null);
+                if (params.threadId && params.threadId !== 'null') {
+                    reqParams.resource.threadId = params.threadId;
+                }
+        
+                gmail.users.messages.send(reqParams, null, function (err, resp) {
+                    if (!err) {
+                        return callback(null, resp);
+                    }
+                    else {
+                        return callback(`Unable to send email: ${err.message}`, null);
+                    }
+                });
             }
         });
     });
